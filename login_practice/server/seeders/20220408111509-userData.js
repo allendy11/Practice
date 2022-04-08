48d5535a4a778162c041a1eb499409487ff751a4
Daehee Yoon <allendy11@gmail.com>
2022-04-08 20:28:48 +0900

    login_practice/first commit

---
+++ login_practice/server/seeders/20220408111509-userData.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert("Users", [
+      {
+        username: "allendy11",
+        password: "1234",
+        email: "allendy11@gmail.com",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
