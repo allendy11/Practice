6408fc0ff3338c22e6d37b6680c087b9d3fc2e94
Daehee Yoon <allendy11@gmail.com>
2022-04-27 23:09:30 +0900

    Todo_vue/

---
+++ Todo_vue/client/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <TodoView
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-todo="toggleTodo"
+      @delete-todo="deleteTodo"
+    />
+  </div>
+</template>
+
+<script>
+import TodoView from "@/components/TodoView";
+export default {
+  components: {
+    TodoView,
+  },
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    toggleTodo({ id, checked }) {
+      this.$emit("toggle-todo", { id, checked });
+    },
+    deleteTodo(id) {
+      this.$emit("delete-todo", id);
+    },
+  },
+};
+</script>
+
+<style></style>
