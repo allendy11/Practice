c7799a68b545afd67fd768ab52b8a2927a1e73a5
Daehee Yoon <allendy11@gmail.com>
2022-05-23 23:58:44 +0900

    mysql/practice build server

---
+++ mysql/server/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    host: "localhost",
+    user: "root",
+    password: 1234,
+    database: "server_test",
+  },
+  test: {
+    host: "localhost",
+    user: "root",
+    password: 1234,
+    database: "server_test",
+  },
+};
---
+++ mysql/server/controllers/index.js
@@ -0,0 +1,4 @@
+const models = require("../models");
+module.exports = {
+  user: (req, res) => {},
+};
