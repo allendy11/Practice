4cb280396cdaf39c3a48f30ec597b48a3738a5e2
Daehee Yoon <allendy11@gmail.com>
2022-08-21 18:10:37 +0900

    reset

--- VanillaJS/changeCase_2tier/server/server.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const requestHandler = require("./requestHandler");
-const port = 4000;
-let server = http.createServer(requestHandler);
-server.listen(port, () => {
-  console.log(`listen on ${port}`);
-});
