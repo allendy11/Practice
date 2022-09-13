6798fbb6419c4496e1f75a88240f0faa250e430d
allendy11 <allendy11@gmail.com>
2022-09-13 11:22:33 +0900

    [NCBIIndexer_C++] add lucene++

---
+++ lucene++_test/src/lucene++_test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lucene++_test.cpp
+// Author      : Dae-hee Yoon @Neuroears Ltd.
+// Version     :
+// Copyright   : All rights reserved by Dae-hee Yoon and Neuroears Ltd.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
