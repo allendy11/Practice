ae9eb9f491a68fb6fb97a440b984661e657db06b
allendy11 <allendy11@email.com>
2022-10-17 19:41:01 +0900

    [triaina_pandas] triaina_macos: make process efficiently

--- triaina_pandas_macos/preprocess/create_taxa_meta.py
+++ triaina_pandas_macos/preprocess/create_taxa_meta.py
@@ -9,7 +9,7 @@ import pandas as pd
 from preprocess.FTPConnection import FTPConnection
 
 
-def create_taxa_meta(taxa):
+def download_summary(taxa):
     host = "ftp.ncbi.nlm.nih.gov"
     ftp_con = FTPConnection()
     ftp_con.set_host(host)
@@ -43,13 +43,13 @@ def create_taxa_meta(taxa):
 
 
 def set_local_path(ftp_path, taxa):
-    output_dir = f"/Volumes/study/data/NCBI/{taxa}"
+    local_dir = f"/Volumes/study/data/NCBI/{taxa}"
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not os.path.exists(local_dir):
+        os.makedirs(local_dir)
 
     file_name = os.path.basename(ftp_path)
-    local_path = f"{output_dir}/{file_name}_genomic.fna.gz"
+    local_path = f"{local_dir}/{file_name}_genomic.fna.gz"
     return local_path
 
 
@@ -61,37 +61,66 @@ def modify_taxa_meta(df, taxa):
     return df
 
 
-def is_downloaded(row, ftp_con, taxa, p_id, df_len):
-    if p_id == None:
-        print(taxa, end=" ")
-        print(row.name, end=" ")
+def is_downloaded(row, ftp_con, taxa, p_id):
+    if p_id != None:
+        print(f"{taxa}_{p_id} {row.name}", end='')
     else:
-        print(f"{taxa}-{p_id}", end=" ")
-        print(f"{int(row.name) - (int(p_id) * int(df_len))}", end=" ")
+        print(f"{taxa} {row.name}", end='')
 
     output_path = row["local_path"]
     file_name = os.path.basename(output_path)
-    print(file_name)
-    remote_dir = row["ftp_path"].split(sep='/', maxsplit=3)
-    remote_path = f"{remote_dir[-1]}/{file_name}"
+    remote_dir = (row["ftp_path"].split(sep='/', maxsplit=3))[-1]
+    remote_path = f"{remote_dir}/{file_name}"
     remote_size = ftp_con.get_size(remote_path)
 
     if os.path.exists(output_path):
         local_size = os.path.getsize(output_path)
-
-        if local_size == remote_size:
-            print("pass")
+        if remote_size == local_size:
+            print("TRUE")
             return True
-        else:
-            print("update...")
-    else:
-        print("not exist")
+    print("FALSE")
+    return False
 
+
+def check_is_downloaded(df, taxa, p_id=None):
+    df_len = len(df)
+    if p_id == None:
+        print(f"{taxa} total: {df_len}")
+    else:
+        print(f"{taxa}_{p_id} total: {df_len}")
+
+    host = "ftp.ncbi.nlm.nih.gov"
+    ftp_con = FTPConnection()
+    ftp_con.set_host(host)
+    ftp_con.connect()
+
+    df["is_downloaded"] = df.apply(
+        lambda row: is_downloaded(row, ftp_con, taxa, p_id), axis=1)
+    ftp_con.close()
+
+    list = ["assembly_accession", "taxid", "species_taxid", "organism_name",
+            "infraspecific_name", "ftp_path", "local_path", "is_downloaded"]
+    view = df[list]
+    df = view.copy()
+    return df[df["is_downloaded"] == False]
+
+
+def data_download(row, ftp_con, taxa, p_id, df_len):
+
+    output_path = row["local_path"]
+    file_name = os.path.basename(output_path)
+    remote_dir = row["ftp_path"].split(sep='/', maxsplit=3)
+    remote_path = f"{remote_dir[-1]}/{file_name}"
+
+    if p_id != None:
+        print(f"{taxa}_{p_id} {row.name} : {file_name}")
+    else:
+        print(f"{taxa} {row.name} : {file_name}")
     ftp_con.download(output_path, remote_path)
+
     print("downloaded")
 
-    local_size = os.path.getsize(output_path)
-    return local_size == remote_size
+    return True
 
 
 def create_data_file(file_type, df, taxa, p_id):
@@ -109,30 +138,32 @@ def create_data_file(file_type, df, taxa, p_id):
 
 def download_taxa_data(df, taxa, p_id=None):
     df_len = len(df)
+    if df_len == 0:
+        return df
     if p_id == None:
-        print(f"OneCore-{df_len}")
+        print(f"{taxa} total: {df_len}")
     else:
-        print(f"core{p_id}-{df_len}")
+        print(f"{taxa}_{p_id} total: {df_len}")
+
     host = "ftp.ncbi.nlm.nih.gov"
     ftp_con = FTPConnection()
     ftp_con.set_host(host)
     ftp_con.connect()
 
-    df["is_downloaded"] = df.apply(lambda row: is_downloaded(
+    df["is_downloaded"] = df.apply(lambda row: data_download(
         row, ftp_con, taxa, p_id, df_len), axis=1)
 
     ftp_con.close()
 
-    list = ["assembly_accession", "taxid", "species_taxid", "organism_name",
-            "infraspecific_name", "ftp_path", "local_path", "is_downloaded"]
-    create_data_file("tsv", df, taxa, p_id)
-    return df[list]
+    # create_data_file("tsv", df, taxa, p_id)
+    return df
 
 
 if __name__ == '__main__':
     taxa_list = ["archaea", "fungi", "viral", "protozoa", "bacteria"]
     for taxa in taxa_list:
-        df = create_taxa_meta(taxa)
+        df = download_summary(taxa)
         df = modify_taxa_meta(df, taxa)
-        df = download_taxa_data(df, taxa)
+        df = check_is_downloaded(df, taxa)
+        df = download_taxa_data(df)
         break
--- triaina_pandas_macos/preprocess/parallel_process.py
+++ triaina_pandas_macos/preprocess/parallel_process.py
@@ -3,24 +3,36 @@ Created on 14 Oct 2022
 
 @author: allen
 '''
-
+import time
 import multiprocessing
 import numpy as np
 import pandas as pd
 from joblib import delayed, Parallel
-from preprocess.create_taxa_meta import create_taxa_meta
-from preprocess.create_taxa_meta import modify_taxa_meta
-from preprocess.create_taxa_meta import download_taxa_data
+from preprocess.create_taxa_meta import download_summary, modify_taxa_meta, check_is_downloaded, download_taxa_data
 
 if __name__ == '__main__':
     taxa_list = ["archaea", "fungi", "viral", "protozoa", "bacteria"]
-
+    taxa_list = ["fungi", "viral", "protozoa", "bacteria"]
     n_procs = multiprocessing.cpu_count()
-
     for taxa in taxa_list:
-        df = create_taxa_meta(taxa)
+        df = download_summary(taxa)
         df = modify_taxa_meta(df, taxa)
-        dfs = np.array_split(df, n_procs)
-        result_dfs = Parallel(n_jobs=n_procs)(delayed(download_taxa_data)(
-            p_df, taxa, p_id) for p_id, p_df in enumerate(dfs))
+        dfs_1 = np.array_split(df, n_procs)
+
+        start = time.time()
+
+        result_dfs = Parallel(n_jobs=n_procs)(delayed(check_is_downloaded)(
+            p_df, taxa, p_id) for p_id, p_df in enumerate(dfs_1))
         gathered_df = pd.concat(result_dfs, axis=0)
+
+        dfs_2 = np.array_split(gathered_df, n_procs)
+        result_dfs_2 = Parallel(n_jobs=n_procs)(delayed(download_taxa_data)(
+            p_df, taxa, p_id) for p_id, p_df in enumerate(dfs_2))
+        gathered_df_2 = pd.concat(result_dfs_2, axis=0)
+
+        end = time.time()
+
+        print(f"total downloaded: {len(gathered_df)}")
+        print(f"{end-start:.3f} sec")
+
+        break
